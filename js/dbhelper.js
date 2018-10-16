7e6ccb63341476a05cf319870dd5b07db9280f2e
Sarah Waldie <sareliz126@gmail.com>
2018-10-16 16:49:58 -0400

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -150,7 +150,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`https://sarahew1206.github.io/FEND-restaurant-proj5/img/${restaurant.photograph}`);
   }
 
   /**
